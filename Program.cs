254020e333b4c1ace0f2e1ed00d5c17f634c3998
Zadok Joshua <65626254+Tsadhoq@users.noreply.github.com>
2022-04-29 12:19:28 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,16 +1,17 @@
-﻿using Exceptions;
+using Exceptions;
+
 Console.WriteLine("******************************************");
 Console.WriteLine("What kind of exception do you want to try?");
 Console.WriteLine("1. Divide By Zero Exception");
 Console.WriteLine("2. Index out of bound Exception");
 Console.WriteLine("3. Invalid Cast Exception");
-Console.WriteLine("4. Null Reference Exception");
-Console.WriteLine("5. Array Type Mismatch Exception");
-Console.WriteLine("6. IO Exception");
-Console.WriteLine("7. Argument Exception");
-Console.WriteLine("8. User Defined Exception One");
-Console.WriteLine("8. User Defined Exception Two");
-Console.WriteLine("8. User Defined Exception Three");
+//Console.WriteLine("4. Null Reference Exception");
+//Console.WriteLine("5. Array Type Mismatch Exception");
+//Console.WriteLine("6. IO Exception");
+//Console.WriteLine("7. Argument Exception");
+//Console.WriteLine("8. User Defined Exception One");
+//Console.WriteLine("9 User Defined Exception Two");
+Console.WriteLine("4. User Defined Exception Test");
 Console.WriteLine("******************************************");
 Console.Write("Your Option: ");
 int option = Convert.ToInt32(Console.ReadLine());
@@ -21,5 +22,16 @@ switch (option)
 {
     case 1:
         demoClass.DividedbyZero();
+        break;
+
+    case 2:
+        demoClass.IOutBException();
+        break;
+
+    case 3: 
+        demoClass.InvalidCasException();
+        break;
+    case 4:
+        demoClass.DataSub();
         break;
 }
