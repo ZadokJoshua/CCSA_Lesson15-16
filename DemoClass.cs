ec1317ae4b427c9f30af499f79cc5572293ed6c0
Zadok Joshua <ayegbazadok@outlook.com>
2022-04-29 11:09:43 +0100

    Add project files.

---
+++ DemoClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class DemoClass
+    {
+        public void DividedbyZero()
+        {
+            Console.WriteLine("Divide your number by zero - ");
+            Console.Write("Enter a number: ");
+            int _result = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int ouput = _result / 0;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("I divided your number by zero");
+            }
+        }
+
+        public void IOutBException()
+        {
+            int[] _result = new int[3] {1, 3, 5};
+
+            foreach (int i in _result)
+            {
+
+            }
+
+            Console.WriteLine("I want to add your number to this array - ");
+            Console.Write("Enter a number: ");
+            int _result = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                int ouput = _result / 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("I divided your number by zero");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Exceptions;
+Console.WriteLine("******************************************");
+Console.WriteLine("What kind of exception do you want to try?");
+Console.WriteLine("1. Divide By Zero Exception");
+Console.WriteLine("2. Index out of bound Exception");
+Console.WriteLine("3. Invalid Cast Exception");
+Console.WriteLine("4. Null Reference Exception");
+Console.WriteLine("5. Array Type Mismatch Exception");
+Console.WriteLine("6. IO Exception");
+Console.WriteLine("7. Argument Exception");
+Console.WriteLine("8. User Defined Exception One");
+Console.WriteLine("8. User Defined Exception Two");
+Console.WriteLine("8. User Defined Exception Three");
+Console.WriteLine("******************************************");
+Console.Write("Your Option: ");
+int option = Convert.ToInt32(Console.ReadLine());
+
+DemoClass demoClass = new DemoClass();
+
+switch (option)
+{
+    case 1:
+        demoClass.DividedbyZero();
+        break;
+}
